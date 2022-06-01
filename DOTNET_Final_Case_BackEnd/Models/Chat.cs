0d6f43cd7018c29b323a1ba5b9f47d024c5db56a
VictorRosaValdez <victorrosavaldez@hotmail.com>
2022-06-01 15:40:29 +0200

    CHORE: DbContext

---
+++ DOTNET_Final_Case_BackEnd/Models/Chat.cs
@@ -0,0 +1,13 @@
+﻿
+
+namespace DOTNET_Final_Case_BackEnd.Models
+{
+    public class Chat
+    {
+        // Properties
+        public int ChatId { get; set; }
+
+        // Navegation property
+        public int ProjectId { get; set; }
+    }
+}
--- DOTNET_Final_Case_BackEnd/Models/Project.cs
+++ DOTNET_Final_Case_BackEnd/Models/Project.cs
@@ -1,6 +1,28 @@
-﻿namespace DOTNET_Final_Case_BackEnd.Models
+﻿
+using System.ComponentModel.DataAnnotations;
+
+namespace DOTNET_Final_Case_BackEnd.Models
 {
     public class Project
     {
+        // Properties
+        public int ProjectId { get; set; }
+        [MaxLength(60)] public string Title { get; set; }
+
+        [MaxLength(500)] public string Description { get; set; }
+
+        [MaxLength(60)] public string Theme { get; set; }
+
+        [MaxLength(60)] public string Industry { get; set; }
+
+        [MaxLength(500)] public string Link { get; set; }
+
+        [MaxLength(500)] public string Screen { get; set; }
+
+        [MaxLength(500)] public string Photo { get; set; }
+
+        [MaxLength(60)] public string Progress { get; set; }
+
+
     }
 }
